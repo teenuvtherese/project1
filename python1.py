c875672cc47f39ff839edc8e6dd46b24a0622d4d
teenuvtherese <teenuvtherese@amaljyothi.ac.in>
2018-01-25 11:53:55 +0530

    Update python1.py

--- python1.py
+++ python1.py
@@ -1,6 +1,1 @@
 print ("hello world");
-if __name__ == '__main__':
-    # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
-# [END app]
